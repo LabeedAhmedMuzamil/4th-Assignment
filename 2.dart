caed2ec7802e09710316374066a79a67959d5253
Labeed Ahmed Muzamil <engr.labeed.shaikh@gmail.com>
2021-12-06 10:45:50 +0500

    first commit

---
+++ 2.dart
@@ -0,0 +1,63 @@
+void main() {
+  // //   1)
+
+  // List arr = ["Asad", "Ali", "Labeed"];
+  // print(arr);
+
+  // //  2)
+
+  // List arr2 = [1, 2, 3, "Asad", "Ali", "Labeed"];
+  // print(arr2);
+
+  // //  3)
+
+  // List Pakistan = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.phil.", "PhD"];
+  // print(Pakistan);
+
+  //4)
+
+  // List name = ["Asad", "Ali", "Labeed", "Uzain", "Faheem"];
+  // List obtmarks = [400, 420, 480, 410, 430];
+
+  // print("Name \t obtained Marks \t Persentage");
+  // print("______________________________________");
+  // print("${name[0]} \t\t ${obtmarks[0]} \t\t ${(obtmarks[0] / 500) * 100}");
+  // print("${name[1]} \t\t ${obtmarks[1]}\t \t ${(obtmarks[1] / 500) * 100}");
+  // print("${name[2]} \t\t ${obtmarks[2]}\t \t ${(obtmarks[2] / 500) * 100}");
+  // print("${name[3]} \t\t ${obtmarks[3]}\t \t ${(obtmarks[3] / 500) * 100}");
+  // print("${name[4]} \t\t ${obtmarks[4]}\t \t ${(obtmarks[4] / 500) * 100}");
+
+  //5
+
+  // List assending_score = [400, 420, 480, 410, 430];
+  // assending_score.sort();
+  // print(assending_score);
+
+  //6
+
+  // List deseding_score = [400, 420, 480, 410, 430];
+  // deseding_score.sort();
+  // print(deseding_score.reversed);
+
+  //7
+
+//   List programming_language = [
+//     "C",
+//     "Java",
+//     "Python",
+//     "C++",
+//     "JavaScript",
+//     "PHP",
+//     "SQL"
+//   ];
+//   if (programming_language[2].length > 0) {
+//     programming_language.replaceRange(2, 3, ["Dart"]);
+//     print(programming_language);
+//   }
+// second if use because this is second method both methods are possible
+// i cant use else because else is not working here if index is not present is not runs else shows error
+//   if (programming_language[2] != null) {
+//     programming_language.replaceRange(2, 3, ["Dart2"]);
+//     print(programming_language);
+//   }
+}
